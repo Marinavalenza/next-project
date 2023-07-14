c33117ee41aae2fdb2dee7edbb0cbaae08a6240c
Marinavalenza <marina.valenza014@gmail.com>
2023-07-14 18:17:26 +0200

    first commit

---
+++ src/pages/products/[id].jsx
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+
+export default function (props) {
+  const router = useRouter();
+  const onHandleClick = () => router.back();
+
+  return (
+    <>
+      <h1>{props.data.title}</h1>
+      <button onClick={onHandleClick}>Go back</button>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const queryId = context.query.id;
+
+  const res = await fetch(`https://dummyjson.com/products/ ${queryId}`);
+  const data = res.status === 200 ? await res.json() : {};
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
